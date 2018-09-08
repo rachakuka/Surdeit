0a9c23c92073d97f3bb9ed6ccb7cf4dfc5c83deb
rachakuka <43092853+rachakuka@users.noreply.github.com>
2018-09-08 11:35:34 -0300

    Big bang
    Big bang
    
    Co-Authored-By: rachakuka <rachakuka@users.noreply.github.com>

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float mSpeed;
+	void Start ()
+    {
+        mSpeed = 50f;
+		
+	}
+	
+	void Update ()
+    {
+        transform.Translate(mSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, mSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
+
+
